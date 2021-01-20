c2831240ee603a633bb8e5cbcf72fb3e35df0ff8
Louis <louis.barre.info@gmail.com>
2021-01-20 16:58:18 +0100

    add new webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -62,18 +62,18 @@ Encore
 
     //enable PostCSS
     .enablePostCssLoader()
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
+// uncomment if you use React
+//.enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 ;
 
 if (Encore.isProduction()) {
@@ -88,5 +88,3 @@ if (Encore.isProduction()) {
 }
 ;
 module.exports = Encore.getWebpackConfig();
-
-
